3b62756cffc8f35a43bfa994040f43ee428a8a35
jainisuni <42873541+jainisuni@users.noreply.github.com>
2019-03-21 18:29:00 +0530

    Update BubbleSort.cpp

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -14,7 +14,7 @@ void swap(int &x,int &y)
 void BubbleSort(int arr[],int n)
 {
     for(int i=0;i<n;i++)
-    for(int j=0;j<n;j++)
+    for(int j=0;j<n-i-1;j++)
     {
         if(arr[j]>arr[j+1])
         swap(arr[j],arr[j+1]);
